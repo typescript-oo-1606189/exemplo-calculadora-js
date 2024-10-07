9e225d0a8302590c9cc49dcb24a580b80060b4e4
Jefferson Cardoso <jeffersoncardoso@umov.me>
2024-10-06 21:35:28 -0300

    Adicionando template da calculadora

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # exemplo-calculadora-js
+
+# Rodando o projeto localmente
+
+### Instale o live-server
+npm install -g live-server
+
+live-server
+
+
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 //Lógica da calculadora - somar, subtrair, multiplicar e dividir
 
 //Manipulação da tela - pegar elementos da tela e colocar resultado no input
+
+//Registrar eventos da tela
